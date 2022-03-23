9b86f08020031da243e2ff0255e619a7d0c56693
Alex <>
2022-03-23 18:13:31 -0500

    create folders for commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,7 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 import frc.robot.commands.*;
+import frc.robot.commands.main.*;
+import frc.robot.commands.climbing.*;
 
 public class OI {
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,8 +10,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+
 import frc.robot.subsystems.*;
-import frc.robot.commands.*;
+import frc.robot.commands.main.DriveCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -35,7 +36,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     autonChooser.setDefaultOption("Do Nothing", new InstantCommand());
-    // m_chooser.addOption("My Auto", new MyAutonomousCommand());
+    // autonChooser.addOption("My Auto", new MyAutonomousCommand());
     SmartDashboard.putData("Auto choices", autonChooser);
   }
 
