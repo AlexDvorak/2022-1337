2bdaa692a138765044cdbaa5cb2649b4e7176345
Alex <>
2022-03-23 19:46:11 -0500

    add basic controller class to make OI simpler and less noisy

---
+++ src/main/java/frc/robot/Controller.java
@@ -0,0 +1,69 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class Controller {
+
+  public final XboxController controller;
+
+  private final double kTriggerDeadzone;
+
+  public final JoystickButton ButtonA, ButtonB, ButtonX, ButtonY;
+  public final JoystickButton BumperLeft, BumperRight;
+  public final JoystickButton ButtonStart, ButtonBack;
+
+  /** Allows for using the triggers as buttons, for example <pre>m_Controller.LeftTrigger.whenHeld(new CommandToRun()) */
+  public final Button LeftTrigger, RightTrigger;
+
+  /**
+   * @param port The port index on the Driver Station that the controller is plugged into.
+   * @param triggerDeadzone The minimum amount the trigger must be pressed (from 0.0 to 1.0) for it to be registered as a button press
+   */
+  public Controller(int port, double triggerDeadzone) {
+    this.kTriggerDeadzone = triggerDeadzone;
+
+    controller = new XboxController(port);
+
+    ButtonA = new JoystickButton(controller, XboxController.Button.kA.value);
+    ButtonB = new JoystickButton(controller, XboxController.Button.kB.value);
+    ButtonX = new JoystickButton(controller, XboxController.Button.kX.value);
+    ButtonY = new JoystickButton(controller, XboxController.Button.kY.value);
+
+    BumperLeft = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
+    BumperRight = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
+
+    ButtonStart = new JoystickButton(controller, XboxController.Button.kStart.value);
+    ButtonBack = new JoystickButton(controller, XboxController.Button.kBack.value);
+
+    LeftTrigger = new Button(() -> getLeftTrigger() > kTriggerDeadzone);
+    RightTrigger = new Button(() -> getRightTrigger() > kTriggerDeadzone);
+  }
+
+  /**
+   * @param port The port index on the Driver Station that the controller is plugged into.
+   */
+  public Controller(int port) {
+    this(port, 0.05); // default trigger deadzone of 0.05;
+  }
+
+  /** The X (left/right) position of the right joystick on the controller from -1.0 to 1.0 */
+  public double getRightStickX() { return controller.getRightX(); }
+
+  /** The Y (up/down) position of the right joystick on the controller from -1.0 to 1.0 */
+  public double getRightStickY() { return controller.getRightY(); }
+
+  /** The X (left/right) position of the left joystick on the controller from -1.0 to 1.0 */
+  public double getLeftStickX() { return controller.getLeftX(); }
+
+  /** The Y (up/down) position of the left joystick on the controller from -1.0 to 1.0 */
+  public double getLeftStickY() { return controller.getLeftY(); }
+
+  /** How much the right trigger on the controller is pressed from 0.0 to 1.0 */
+  public double getRightTrigger() { return controller.getRightTriggerAxis(); }
+
+  /** How much the left trigger on the controller is pressed from 0.0 to 1.0 */
+  public double getLeftTrigger()  { return controller.getLeftTriggerAxis(); }
+
+}
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,44 +4,28 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-import frc.robot.commands.*;
 import frc.robot.commands.main.*;
 import frc.robot.commands.climbing.*;
 
 public class OI {
 
-  public static final XboxController driver = new XboxController(0);
-  public static final XboxController operator = new XboxController(1);
-
-  // Driver Buttons
-  public static final JoystickButton driverBButton = new JoystickButton(driver, XboxController.Button.kB.value);
-  public static final JoystickButton driverAButton = new JoystickButton(driver, XboxController.Button.kA.value);
-  public static final JoystickButton driverYButton = new JoystickButton(driver, XboxController.Button.kY.value);
-  public static final JoystickButton driverXButton = new JoystickButton(driver, XboxController.Button.kX.value);
-
-  // Operator Buttons
-  public static final JoystickButton operatorBButton = new JoystickButton(operator, XboxController.Button.kB.value);
-  public static final JoystickButton operatorAButton = new JoystickButton(operator, XboxController.Button.kA.value);
-  public static final JoystickButton operatorYButton = new JoystickButton(operator, XboxController.Button.kY.value);
-  public static final JoystickButton operatorXButton = new JoystickButton(operator, XboxController.Button.kX.value);
+  public static final Controller driver = new Controller(0);
+  public static final Controller operator = new Controller(1);
 
   public static void mapButtons() {
     // Driver Controls
-    // driverAButton.whenHeld(new RunIntake());
-    // driverYButton.whenHeld(new RunConveyorBelt(1.0));
-    driverBButton.whenHeld(new StartConveyorBelt(1.0));
-    driverXButton.whenHeld(new StopConveyorBelt());
+    // driver.ButtonA.whenHeld(new RunIntake());
+    // driver.ButtonY.whenHeld(new RunConveyorBelt(1.0));
+    driver.ButtonB.whenPressed(new StartConveyorBelt(1.0));
+    driver.ButtonX.whenPressed(new StopConveyorBelt());
 
     // Operator Controls
-    operatorAButton.whenPressed(new ChangeClimberDirection());
+    operator.ButtonA.whenPressed(new ChangeClimberDirection());
   }
 
   public static double getDrivingSpeed() {
-    double accelerate = driver.getLeftTriggerAxis();
-    double brake = driver.getRightTriggerAxis();
+    double accelerate = driver.getLeftTrigger();
+    double brake = driver.getRightTrigger();
 
     if (Math.abs(accelerate - brake) > 0.15)
     return accelerate - brake;
@@ -50,7 +34,7 @@ public class OI {
   }
 
   public static double getDrivingTurn() {
-    double turn = driver.getLeftX();
+    double turn = driver.getLeftStickX();
     if (turn > 0.15)
     return turn;
     else
